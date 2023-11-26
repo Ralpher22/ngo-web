4edeaf51275e4ba33a286f24dab22e515367073c
Ralpher22 <rickyralph@gmail.com>
2023-11-26 14:12:25 +0100

    Add gallery.html, modify index.html and script.js, and add styles.css

--- script.js
+++ script.js
@@ -8,7 +8,15 @@ function showMtnMoneyForm() {
 	document.getElementById('orange-money-form').style.display = 'block';
 	document.getElementById('mtn-money-form').style.display = 'none';
   }
-
+  const thumbnails = document.querySelectorAll('.thumbnail');
+  const mainImage = document.querySelector('.main-image');
+  
+  thumbnails.forEach(thumbnail => {
+	  thumbnail.addEventListener('click', () => {
+		  mainImage.src = thumbnail.src;
+	  });
+  });
+  
 
 
  
